8062c525fa2b266c45a02dcd82b613a47c18a1b1
John Koster <john@stillat.com>
2023-11-13 08:20:04 -0600

    Prevent some handlers from always running

--- src/Listeners/StatamicResponseCreated.php
+++ src/Listeners/StatamicResponseCreated.php
@@ -2,6 +2,8 @@
 
 namespace Stillat\StatamicSiteEssentials\Listeners;
 
+use Illuminate\Http\RedirectResponse;
+use Statamic\Statamic;
 use Stillat\StatamicSiteEssentials\View\DeferredExecution\Manager;
 use Stillat\StatamicSiteEssentials\View\ViewObserver;
 
@@ -9,11 +11,26 @@ class StatamicResponseCreated
 {
     public function handle($event)
     {
-        /** @var Manager $deferredManager */
-        $deferredManager = app(Manager::class);
-        $deferredManager->applyToResponse($event->response);
-        $deferredManager->reset();
+        $this->runFrontEndResponseHandlers($event->response);
 
         app(ViewObserver::class)->reset();
+    }
+
+    protected function runFrontEndResponseHandlers($response)
+    {
+        if (! $response) {
+            return;
+        }
+        if ($response instanceof RedirectResponse) {
+            return;
+        }
+        if (Statamic::isCpRoute()) {
+            return;
+        }
+
+        /** @var Manager $deferredManager */
+        $deferredManager = app(Manager::class);
+        $deferredManager->applyToResponse($response);
+        $deferredManager->reset();
     }
 }
