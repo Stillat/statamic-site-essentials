d6fb42b26785a89c9d39e5f0a3ade7e053bbe0c2
John Koster <john@stillat.com>
2023-11-16 18:05:18 -0600

    Update OpenGraphBuilder.php

--- src/Metadata/Builders/OpenGraphBuilder.php
+++ src/Metadata/Builders/OpenGraphBuilder.php
@@ -8,27 +8,52 @@ use Stillat\StatamicSiteEssentials\Metadata\DataHelpers;
 
 class OpenGraphBuilder extends AbstractMetaTagBuilder
 {
+    /**
+     * Queues a <meta property="og:title" content="..."> tag.
+     *
+     * Variable: $title
+     */
     public function title(mixed $title = '$title'): self
     {
         return $this->property('og:title', $title);
     }
 
+    /**
+     * Queues a <meta property="og:type" content="..."> tag.
+     *
+     * Defaults to "website".
+     */
     public function type($type = 'website'): self
     {
         return $this->property('og:type', $type);
     }
 
-    public function url($url = '$current_url'): self
+    /**
+     * Queues a <meta property="og:url" content="..."> tag.
+     *
+     * Variable: $current_url
+     */
+    public function url($url = '$current_full_url'): self
     {
         return $this->property('og:url', $url);
     }
 
-    public function description($description = ''): self
+    /**
+     * Queues a <meta property="og:description" content="..."> tag.
+     *
+     * Variable: $meta_description
+     */
+    public function description($description = '$meta_description'): self
     {
         return $this->property('og:description', $description);
     }
 
-    public function siteName($siteName = ''): self
+    /**
+     * Queues a <meta name="site_name" property="og:site_name" content="..."> tag.
+     *
+     * Variable: $meta_site_name
+     */
+    public function siteName($siteName = '$meta_site_name'): self
     {
         return $this->add([
             'name' => 'site_name',
@@ -37,6 +62,11 @@ class OpenGraphBuilder extends AbstractMetaTagBuilder
         ]);
     }
 
+    /**
+     * Queues a <meta property="og:locale" content="..."> tag.
+     *
+     * Defaults to the current site's locale.
+     */
     public function locale($locale = null): self
     {
         if ($locale == null) {
@@ -48,6 +78,9 @@ class OpenGraphBuilder extends AbstractMetaTagBuilder
         return $this->property('og:locale', $locale);
     }
 
+    /**
+     * Queues a <meta property="og:locale:alternate" content="..."> tag.
+     */
     public function localeAlternate($localeAlternate = null): self
     {
         if ($localeAlternate == null) {
@@ -57,6 +90,9 @@ class OpenGraphBuilder extends AbstractMetaTagBuilder
         return $this->property('og:locale:alternate', $localeAlternate);
     }
 
+    /**
+     * Queues a <meta name="image" property="og:image" content="..."> tag.
+     */
     public function image($image = ''): self
     {
         return $this->add([
@@ -66,6 +102,15 @@ class OpenGraphBuilder extends AbstractMetaTagBuilder
         ]);
     }
 
+    /**
+     * Uses information from the provided asset to queue the following tags:
+     *
+     * <meta name="image" property="og:image" content="...">
+     * <meta property="og:image:width" content="...">
+     * <meta property="og:image:height" content="...">
+     * <meta property="og:image:type" content="...">
+     * <meta property="og:image:alt" content="...">
+     */
     public function imageAsset(?Asset $asset): self
     {
         if (! $asset) {
@@ -79,6 +124,14 @@ class OpenGraphBuilder extends AbstractMetaTagBuilder
             ->imageAlt($asset->get('alt'));
     }
 
+    /**
+     * Uses information from the provided asset to queue the following tags:
+     *
+     * <meta name="video" property="og:video" content="...">
+     * <meta property="og:video:type" content="...">
+     * <meta property="og:video:width" content="...">
+     * <meta property="og:video:height" content="...">
+     */
     public function videoAsset(?Asset $asset): self
     {
         if (! $asset) {
@@ -91,26 +144,41 @@ class OpenGraphBuilder extends AbstractMetaTagBuilder
             ->videoHeight($asset->height());
     }
 
+    /**
+     * Queues a <meta property="og:image:type" content="..."> tag.
+     */
     public function imageType($imageType = ''): self
     {
         return $this->property('og:image:type', $imageType);
     }
 
+    /**
+     * Queues a <meta property="og:image:width" content="..."> tag.
+     */
     public function imageWidth($imageWidth = ''): self
     {
         return $this->property('og:image:width', $imageWidth);
     }
 
+    /**
+     * Queues a <meta property="og:image:height" content="..."> tag.
+     */
     public function imageHeight($imageHeight = ''): self
     {
         return $this->property('og:image:height', $imageHeight);
     }
 
+    /**
+     * Queues a <meta property="og:image:alt" content="..."> tag.
+     */
     public function imageAlt($imageAlt = ''): self
     {
         return $this->property('og:image:alt', $imageAlt);
     }
 
+    /**
+     * Queues a <meta name="video" property="og:video" content="..."> tag.
+     */
     public function video($video = ''): self
     {
         return $this->add([
@@ -120,16 +188,25 @@ class OpenGraphBuilder extends AbstractMetaTagBuilder
         ]);
     }
 
+    /**
+     * Queues a <meta property="og:video:type" content="..."> tag.
+     */
     public function videoType($videoType = ''): self
     {
         return $this->property('og:video:type', $videoType);
     }
 
+    /**
+     * Queues a <meta property="og:video:width" content="..."> tag.
+     */
     public function videoWidth($videoWidth = ''): self
     {
         return $this->property('og:video:width', $videoWidth);
     }
 
+    /**
+     * Queues a <meta property="og:video:height" content="..."> tag.
+     */
     public function videoHeight($videoHeight = ''): self
     {
         return $this->property('og:video:height', $videoHeight);
